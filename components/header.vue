20f728ea923376e4b24b2c9bc3880f85ab785d40
Edson Dos Santos <djiedson413@gmail.com>
2021-04-06 07:19:00 +0100

    Plataforma de video

---
+++ components/header.vue
@@ -0,0 +1,37 @@
+<template>
+    
+    <div class="header">
+
+        <div class="container d-flex">
+
+            <div class="logo">
+                <h1>
+                    <a href="">VideoPlatform</a>
+                </h1>
+            </div>
+
+            <ul class="nav d-flex">
+                <li>
+                    <a href="">Cursos</a>
+                </li>
+                <li>
+                     <a href="">Conta</a>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+import '../assets/css/header.css'
+export default {
+    name: "header"
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import navBar from "../components/header.vue"
+import '../assets/css/root.css'
+export default {
+  components:{
+    navBar
+  }
+}
+</script>
+
+<style>
+
+</style>
